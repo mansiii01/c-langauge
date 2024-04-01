f54b2482c45743c91927e0abda63b48a4ae69cfb
mansii0119 <141994697+mansiii01@users.noreply.github.com>
2024-04-01 17:06:51 +0530

    Add files via upload

---
+++ 2D arr/task2.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+main(){
+    
+    int sum;
+    int arr[3][3] = { 
+        {2, 4, 1},
+        {3, 5, 4},
+        {8, 2, 6}};
+
+    for (int i = 0; i <= 2; i++)
+    {
+        for (int j = 0; j <= 2; j++)
+        {
+            if(i == j){
+                printf("%d",arr[i][j]);
+                sum += arr[i][j];
+            }
+        }
+        printf("\n");
+        
+    }
+    printf("%d",sum);
+
+}
