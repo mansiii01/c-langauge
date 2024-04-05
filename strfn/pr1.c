5ed9986e979d2eb6fc3da073ee061e7f9bca963a
mansii0119 <141994697+mansiii01@users.noreply.github.com>
2024-04-05 16:19:55 +0530

    Add files via upload

---
+++ strfn/pr1.c
@@ -0,0 +1,56 @@
+#include<stdio.h>
+main(){
+	
+//	Que1
+
+	char n[100];
+	printf("Enter your name : ");
+	gets(n);
+	puts(strupr(n));
+	
+//	Que2
+
+//	char n[100];
+//	printf("Enter your name : ");
+//	gets(n);
+//	puts(strlwr(n));
+
+//	Que3
+
+//	char n[100];
+//	printf("Enter your name : ");
+//	gets(n);
+//	printf("%d",strlen(n));
+
+//	Que4
+
+//	char n[100];
+//	printf("Enter your name : ");
+//	gets(n);
+//	puts(n);
+
+//	Que5
+
+//	char f[100],l[100],fullname[100];
+//	printf("Enter your firstname : ");
+//	gets(f);
+//	printf("Enter your lastname : ");
+//	gets(l);		
+//	strcpy(fullname , strcat(f , l));
+//	puts(fullname);	
+
+//	Que6
+
+//    char m[100],n[100];
+//    gets(m);
+//    gets(n);
+//
+//
+//	if(strcmp(m,n) == 0){
+//		printf("Strings are equal");
+//	}
+//	else{
+//		printf("No, they are not equal");
+//    }
+
+}
